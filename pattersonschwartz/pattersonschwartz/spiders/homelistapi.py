9f1c7c444b06a99dc19af55c12b4c6f529662c50
click-here <ragespectate@gmail.com>
2019-03-23 17:53:41 -0400

    added API method

---
+++ pattersonschwartz/pattersonschwartz/spiders/homelistapi.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+class HomelistapiSpider(scrapy.Spider):
+    name = 'homelistapi'
+    allowed_domains = ['pattersonschwartz.com']
+    page = 1
+    api_url = 'http://www.pattersonschwartz.com/api/ps/forsale/Cecil,Harford/priceMin_225000/priceMax_600000/page_{}'
+    start_urls = [api_url.format(page)]
+
+    def parse(self, response):
+        data = json.loads(response.text)
+        for r in data['thumbs']:
+            yield{
+                'id' : r['id'],
+                'address' : r['a'],
+                'price' : r['p'],
+                'cdp' : r['c'],
+                'picture' : r['pu'],
+                'vt': r['vt'],
+                'cb': r['cb'],
+                'f': r['f'],
+                'nl': r['nl'],
+                'np': r['np'],
+                'oh' : r['oh'],
+                'd': r['d']
+            }
+        if not data['isLastPage']:
+            self.page += 1
+            yield scrapy.Request(url=self.api_url.format(self.page), callback=self.parse)
+
